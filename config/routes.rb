6762df4c78e7268da8b8826333a0db65c5799305
Ariane Coulon <ariane.coulon@gmail.com>
2016-02-09 13:59:47 +0100

    todolist

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'tasks#index'
   resources :tasks, except: :destroy
 
   # The priority is based upon order of creation: first created -> highest priority.
